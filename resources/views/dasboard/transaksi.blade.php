6cb7217aed663eb769cbca8235524d419c51062a
arisygdc <arisycso@gmail.com>
2023-03-22 13:04:29 +0700

    transaksi not yet implement

--- resources/views/dasboard/transaksi.blade.php
+++ resources/views/dasboard/transaksi.blade.php
@@ -26,14 +26,16 @@
                                         </tr>
                                     </thead>
                                     <tbody>
+                                        <!-- @foreach ($transaksi as $val) -->
                                         <tr>
-                                            <td>Tiger Nixon</td>
-                                            <td>System Architect</td>
-                                            <td>Edinburgh</td>
+                                            <td>Not Implemented</td>
+                                            <td>Not Implementedt</td>
+                                            <td>Not Implemented</td>
                                             <td>61</td>
                                             <td>2011/04/25</td>
                                             <td>$320,800</td>
                                         </tr>
+                                        <!-- @endforeach -->
                                     </tbody>
                                 </table>
                             </div>
