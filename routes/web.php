396a2e75137898c20e69dab2c89fad3ff20cb0fb
arisygdc <arisycso@gmail.com>
2023-03-22 10:59:53 +0700

    route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\KaryawanController;
+use App\Http\Controllers\PenggunaController;
+use App\Http\Middleware\KaryawanRole;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TokoController;
 /*
@@ -15,3 +19,15 @@ use App\Http\Controllers\TokoController;
 
 Route::get('/', [TokoController::class, 'index']);
 Route::get('/category', [TokoController::class, 'category']);
+Route::get('/login', [AuthController::class, 'index_login']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::get('/logout', [AuthController::class, 'logout']);
+
+Route::middleware([KaryawanRole::class])->group(function() {
+    Route::get('/karyawan', [KaryawanController::class, 'index']);
+    Route::get('/karyawan/barang', [KaryawanController::class, 'barang']);
+});
+
+Route::name('pengguna.')->group(function() {
+    Route::get('/pengguna', [PenggunaController::class, 'index'])->name('index');
+});
