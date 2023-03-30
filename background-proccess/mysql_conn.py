42a9f2d29129d93a622d30e13385ae8b2cf7454d
arisygdc <arisycso@gmail.com>
2023-03-30 13:25:34 +0700

    runable task

--- background-proccess/mysql_conn.py
+++ background-proccess/mysql_conn.py
@@ -12,6 +12,22 @@ def getConn():
     # create cursor object
     return db
 
+def getPekerjaan():
+    db = getConn()
+    cursor = db.cursor()
+    # read data
+    sql = """
+    select pekerjaan from pengguna group by pekerjaan
+    """
+    cursor.execute(sql)
+    pek = []
+    for row in cursor:
+        for field in row:
+            pek.append(field)
+            
+    cursor.close()
+    db.close()
+    return pek
 
 def InsertRekomendasi():
     # insert data
@@ -25,17 +41,42 @@ def InsertRekomendasi():
     cursor.close()
     db.close()
 
-def selectForRecomendation():
+def penjualanByPekerjaan():
     db = getConn()
     cursor = db.cursor()
     # read data
     sql = """
     SELECT p.pekerjaan as pekerjaan, dt.kode_barang as barang, dt.jumlah
     FROM detail_transaksi dt inner join transaksi t on dt.kode_transaksi = t.id 
-    left join pengguna p on p.kode_pengguna = t.kode_pengguna
+    left join pengguna p on p.kode_pengguna = t.kode_pengguna where t.id > 0
     """
     cursor.execute(sql)
-    results = cursor.fetchall()
+    results = []
+    for row in cursor:
+        coll = []
+        for field in row:
+            coll.append(field)
+        results.append(coll)
+    cursor.close()
+    db.close()
+    return results
+
+def penjualanByUsia():
+    db = getConn()
+    cursor = db.cursor()
+    # read data
+    sql = """
+    SELECT  YEAR(now()) - YEAR(p.tgl_lahir) as old, dt.kode_barang as barang, dt.jumlah
+    FROM detail_transaksi dt inner join transaksi t on dt.kode_transaksi = t.id
+    left join pengguna p on p.kode_pengguna = t.kode_pengguna where t.id > 0
+    """
+    cursor.execute(sql)
+    results = []
+    for row in cursor:
+        coll = []
+        for field in row:
+            coll.append(field)
+        results.append(coll)
     cursor.close()
     db.close()
     return results
--- background-proccess/recomendation.py
+++ background-proccess/recomendation.py
@@ -3,72 +3,31 @@ import sys
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from mysql_conn import selectForRecomendation
+from mysql_conn import *
+from helper import *
 from matplotlib import style
 style.use("ggplot")
 
-rekomendasipek = pd.read_csv("./rekomendasi kmeans FIX.csv", usecols=[
-   "pekerjaan1",
-   "barang1",
-   "Terjual1",
-])
 
-#read csv file with ; separator
-rekomendasipek.head()
+def km1(clusterLabel, x_array):
+    kmeans = KMeans(n_clusters=5, random_state=37, n_init='auto')
+    kmeans.fit(x_array)
+    clusterLabel = kmeans.labels_
 
-rekomendasipek_x = rekomendasipek.iloc[:, 0:3]
-rekomendasipek_x.head()
+
+    from sklearn.metrics import confusion_matrix,classification_report
+    print(confusion_matrix(clusterLabel,kmeans.labels_))
+    print(classification_report(clusterLabel,kmeans.labels_))
+
+pek = getPekerjaan()
+rekomendasipek_x = x_merge(penjualanByPekerjaan(), pek)
+clusterLabel = ['pekerjaan', 'barang', 'jumlah']
 x_array = np.array(rekomendasipek_x)
 
-# Menampilkan data penjualan
-print("---Data Pejualan---")
-print(rekomendasipek)
+km1(clusterLabel, x_array)
 
-# Cek Parameter dari Pengguna
-if len(sys.argv) > 1:
-    nCluster = int(sys.argv[1])
-else:
-    nCluster = 1
+rekomendasiUs_x = penjualanByUsia()
+clusterLabel = ['usia', 'barang', 'jumlah']
+x_array = np.array(rekomendasiUs_x)
 
-kmeans = KMeans(n_clusters=5, random_state=37)
-kmeans.fit(x_array)
-rekomendasipek["cluster"] = kmeans.labels_
-
-
-centroids = kmeans.cluster_centers_
-labels = kmeans.labels_
-
-
-print("---CENTROID---")
-print(centroids)
-
-print("---LABELS---")
-print(labels)
-
-
-
-from sklearn.metrics import confusion_matrix,classification_report
-print(confusion_matrix(rekomendasipek["cluster"],kmeans.labels_))
-print(classification_report(rekomendasipek["cluster"],kmeans.labels_))
-
-#Menampilkan visualization
-fig = plt.figure()
-centers = kmeans.cluster_centers_
-ax = fig.add_subplot(111, projection='3d')
-ax.scatter(x_array[:, 0], x_array[:, 1], x_array[:, 2], c=labels)
-ax.scatter(centers[:, 0], centers[:, 1], centers[:, 2], c="red", s=50, alpha=1, marker="o")
-ax.set_xlabel('Pekerjaan')
-ax.set_ylabel('ID Barang')
-ax.set_zlabel('Terjual')
-
-# Mengelompokkan data berdasarkan label cluster
-grouped_data = penjualan.groupby("cluster")
-
-# Mencari ID Barang yang paling banyak muncul di setiap kelompok
-for label, group in grouped_data:
-    top_product = group["Barang"].value_counts().index[0]
-    print(f"Cluster {label}: ID Barang terbanyak adalah {top_product}")
-
-
-plt.show()
-
+km1(clusterLabel, x_array)
