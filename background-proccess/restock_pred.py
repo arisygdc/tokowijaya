91ba5e90696a2f9f3982f63572ed74addecf6555
arisygdc <arisycso@gmail.com>
2023-04-03 23:58:29 +0700

    restock prediction

---
+++ background-proccess/restock_pred.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+from os import path
+import sys
+print(sys.path.append(path.abspath('../../background-proccess')))
+from mysql_conn import Querier
+from kmeans import Kmeans
+
+querier = Querier()
+predData = querier.barangJumlahTerjual()
+clusterLabel = ['barang', 'jumlah', 'terjual']
+
+# Restock prediction using kmeans
+kmeans = Kmeans(clusterLabel, predData)
+stockers = kmeans.restokcRecomendation()
+for k, v in stockers.items():
+    querier.insertRekomendasiRestock(k, v)
